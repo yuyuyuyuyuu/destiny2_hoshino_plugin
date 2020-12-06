218bb2d8912bc377fac01268dd67da63db8d8365
AZMIAO <54100327+azmiao@users.noreply.github.com>
2020-12-06 14:33:24 +0000

    重构了获取数据方法，并修复了潜在的不严谨性问题

--- get_zhoubao_info.py
+++ get_zhoubao_info.py
@@ -1,28 +1,29 @@
 import re
 import requests
-from .get_cookies import *
 
-# 返回周报专栏的源码
+# 返回小黑盒百科的源码
 def getzhoubaoHtml(url):
-  cookie = {"Cookie": str(get_cookie())}
-  html111 = requests.get(url, cookies = cookie)
+  html111 = requests.get(url)
   html111.encoding = 'utf-8'
   html111 = html111.text
+  html111 = html111.replace('\n','')
+  html111 = html111.replace(' ','')
   # print(html111)
   return html111
 
 # 返回周报的链接
 def getzhoubao(html111):
-  imglist = re.findall(r'(https\:\/\/weibo\.com\/ttarticle\/p\/show.+?class)', html111)
-  for html11 in imglist:
-    html11 = html11.replace('" class','')
-    # print(html11)
-    return html11
+  imglist = re.findall(r'tag\_name\"\:\"\\u5168\\u90e8.+?id\=1085660', html111)
+  for html10 in imglist:
+    imglist = re.findall(r'https\:\\\/\\\/api\.xiaoheihe\.cn\\\/wiki\\\/get\_article\_for\_app.+?id\=1085660', html10)
+    for html11 in imglist:
+      html11 = html11.replace('\\','')
+      # print(html11)
+      return html11
 
 # 返回周报的源码
 def getzhoubaoHtml1(html11):
-  cookie = {"Cookie": str(get_cookie())}
-  html1 = requests.get(html11, cookies = cookie)
+  html1 = requests.get(html11)
   html1.encoding = 'utf-8'
   html1 = html1.text
   # print(html1)
@@ -30,12 +31,11 @@ def getzhoubaoHtml1(html11):
 
 # 字符串格式输出图片链接
 def getzhoubaoImg(html1):
-  imglist = re.findall(r'(img\-box\=\"img\-box\"\ class\=\"picbox\"\>\<img\ src\=\"https\:\/\/wx.\.sinaimg\.cn\/large.+?\.jpg)', html1)
+  imglist = re.findall(r'https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.png|https\:\/\/cdn\.max\-c\.com\/heybox\/dailynews\/img\/(?!c4f5035d1b8053c400c72c0656c12d97).+?\.jpg', html1)
   for url1 in imglist:
-    url1 = url1.replace('img-box="img-box" class="picbox"><img src="','')
     return url1
 
-html111 = str(getzhoubaoHtml("https://weibo.com/a/hot/7583892511954945_1.html"))
+html111 = str(getzhoubaoHtml("https://api.xiaoheihe.cn/wiki/get_homepage_content/?wiki_id=1085660&verison=&is_share=1"))
 html11 = str(getzhoubao(html111))
 html1 = str(getzhoubaoHtml1(html11))
-print(getzhoubaoImg(html1))
+# print(getzhoubaoImg(html1))
